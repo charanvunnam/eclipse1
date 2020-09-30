d2747fb7c5c69190df4fbd2c3687b9ebbd304237
hp <vunnamcharan3734@gmail.com>
2020-10-01 00:44:05 +0530

    commit change

--- github/src/test/java/github/github.java
+++ github/src/test/java/github/github.java
@@ -4,16 +4,20 @@ package github;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class github {
-	public static void main (String args[]) throws InterruptedException
-	{	
+	
+	@Test
+   public void githubTesting() throws InterruptedException{
+	
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\hp\\Desktop\\ec-new\\Selenium2\\src\\test\\resources\\driver\\chromedriver.exe");
 	 WebDriver driver = new ChromeDriver();
 	 driver.get("https://www.facebook.com");
 	 Thread.sleep(1000);
 	 driver.close();
 	 driver.quit();
+
 	}
-	}
+}
 	
