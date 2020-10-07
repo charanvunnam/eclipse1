9a956f0e739250a00300a5d13727c1e1cbe57b8b
hp <vunnamcharan3734@gmail.com>
2020-10-07 16:12:11 +0530

    commit
    Signed-off-by: hp <vunnamcharan3734@gmail.com>

--- github/src/test/java/github/github.java
+++ github/src/test/java/github/github.java
@@ -2,24 +2,49 @@ package github;
 
 
 
+import org.junit.BeforeClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 public class github {
+	ExtentReports extent;
+	WebDriver driver = null;
+	
+	@BeforeTest
+	public void starttest() throws InterruptedException
+	{
+		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("C:\\Users\\hp\\git\\eclipse1\\github\\target\\extendreport\\extent.html");
+		 extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\hp\\Desktop\\ec-new\\Selenium2\\src\\test\\resources\\driver\\chromedriver.exe");
+		 driver = new ChromeDriver();
+	}
 	
 	@Test
    public void githubTesting() throws InterruptedException{
-	
-	System.setProperty("webdriver.chrome.driver","C:\\Users\\hp\\Desktop\\ec-new\\Selenium2\\src\\test\\resources\\driver\\chromedriver.exe");
-	 WebDriver driver = new ChromeDriver();
+     ExtentTest test = extent.createTest("facebooktest","tests a facebook functionality");
+	 test.pass("opening the web browser");
 	 driver.get("https://www.facebook.com");
+	 test.pass("entering the email id");
+	 driver.findElement(By.xpath("//input[@id='email']")).sendKeys("charanvunnam@gmail.com");
+	 test.pass("passing the password");
 	 Thread.sleep(1000);
-	 System.out.println("completed successfully by me");
-	 System.out.println("email is triggered successfully");
+	 driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("Charan1906@");
+	 test.pass("clicking the login button");
+	 driver.findElement(By.xpath("//button[@class='_42ft _4jy0 _6lth _4jy6 _4jy1 selected _51sy']")).click();
+	 Thread.sleep(10000);
+	 test.pass("successfully opened");
 	 driver.close();
 	 driver.quit();
-
+	 test.info("successfully completed the test ");
+	 extent.flush();
 	}
-}
-	
+}	
